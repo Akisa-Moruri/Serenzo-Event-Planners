1b15935d5a1c8671f0093fecbe77dbbddda8a48a
Akisa-Moruri <akisamoruri@gmail.com>
2024-07-11 22:30:22 +0300

    project structure commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Layout from './Layout';
+import NoPage from './pages/NoPage';
+import AddEvent from './pages/AddEvent';
+import Event from './pages/Event';
+import UpdateEvent from './pages/UpdateEvent';
+import EventRegistration from './pages/EventRegistration';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { UserProvider } from './context/UserContext';
+import { EventProvider } from './context/EventContext';
+import Profile from './pages/Profile';
+import Dashboard from './pages/Dashboard';
+import Footer from './pages/Footer';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return (     
+     <BrowserRouter>     
+    <UserProvider>
+      <EventProvider>
+
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path='/' element={<Home />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+
+          <Route path='/event/register' element={<EventRegistration />} />
+          <Route path='/addevent' element={<AddEvent />} />
+          <Route path='/event/:id' element={<Event />} />
+          <Route path='/update/:event_id' element={<UpdateEvent />} />
+
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/profile' element={<Profile />} />
+
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+      
+      </EventProvider>
+    </UserProvider>
+    <Footer />
+    </BrowserRouter>
   )
 }
 
